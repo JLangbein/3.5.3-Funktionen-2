c0ad88594d95df14eadeb05a94751baae2295b2f
Johannes Langbein <jllangbein@gmail.com>
2025-06-20 10:48:09 +0200

    Aufgabe 2

--- main.dart
+++ main.dart
@@ -2,8 +2,17 @@ int trippleSum(int a, int b, int c) {
   return a + b + c;
 }
 
+int textLength(String text) {
+  return text.length;
+}
+
 void main() {
   for (int i = 5; i < 8; i++) {
     print(trippleSum(1, i, i - 1));
+  }
+
+  List<String> texts = ['Rüdiger', 'Nehberg', 'Seine Schwester'];
+  for (String text in texts) {
+    print('Anzahl Zeichen in $text: ${textLength(text)}');
   }
 }
