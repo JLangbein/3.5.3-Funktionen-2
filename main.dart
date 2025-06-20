cb103deed68f3e35005a0461f2a148bda648aad4
Johannes Langbein <jllangbein@gmail.com>
2025-06-20 11:06:15 +0200

    Aufgabe 3

--- main.dart
+++ main.dart
@@ -6,6 +6,16 @@ int textLength(String text) {
   return text.length;
 }
 
+int numberOfVowels(String text) {
+  int count = 0;
+  for (int i = 0; i < text.length; i++) {
+    if (text[i].contains(RegExp(r'[aeiou]', caseSensitive: false))) {
+      count++;
+    }
+  }
+  return count;
+}
+
 void main() {
   for (int i = 5; i < 8; i++) {
     print(trippleSum(1, i, i - 1));
@@ -14,5 +24,6 @@ void main() {
   List<String> texts = ['Rüdiger', 'Nehberg', 'Seine Schwester'];
   for (String text in texts) {
     print('Anzahl Zeichen in $text: ${textLength(text)}');
+    print('Anzahl Vokale in $text: ${numberOfVowels(text)}');
   }
 }
